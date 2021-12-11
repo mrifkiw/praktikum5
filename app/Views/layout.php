02c8dda5ff8db0545e00980d59247d6aae131e49
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2021-12-11 22:56:10 +0700

    feat: add layout as view manager for multiple view

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -18,17 +18,11 @@
 		</div>
 		<div class="hidden px-2 mx-2 navbar-center lg:flex">
 			<div class="flex items-stretch">
-				<a class="btn btn-ghost btn-sm rounded-btn">
+				<a class="btn btn-ghost btn-sm rounded-btn" href="<?= base_url('/') ?>">
 					Home
 				</a>
-				<a class="btn btn-ghost btn-sm rounded-btn">
-					Portfolio
-				</a>
-				<a class="btn btn-ghost btn-sm rounded-btn">
-					About
-				</a>
-				<a class="btn btn-ghost btn-sm rounded-btn">
-					Contact
+				<a class="btn btn-ghost btn-sm rounded-btn" href="<?= base_url('devices') ?>">
+					Devices
 				</a>
 			</div>
 		</div>
@@ -49,19 +43,7 @@
 </nav>
 
 <body>
-	<div class="hero min-h-screen bg-base-200">
-		<div class="text-center hero-content">
-			<div class="max-w-md">
-				<h1 class="mb-5 text-5xl font-bold">
-					Hello there
-				</h1>
-				<p class="mb-5">
-					Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
-				</p>
-				<button class="btn btn-primary">Get Started</button>
-			</div>
-		</div>
-	</div>
+	<?= $this->renderSection('content') ?>
 </body>
 
 </html>
