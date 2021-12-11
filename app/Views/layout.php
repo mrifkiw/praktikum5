b27dfc97eed7b49478feedb3d5410f9198cd329e
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2021-12-11 20:28:02 +0700

    feat: add navbar

---
+++ app/Views/layout.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Welcome to CodeIgniter 4!</title>
+	<meta name="description" content="The small framework with powerful features">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" type="image/png" href="/favicon.ico" />
+	<link rel="stylesheet" href="/css/app.css">
+</head>
+<nav>
+	<div class="navbar shadow-lg bg-purple-400 text-neutral-content">
+		<div class="px-2 mx-2 navbar-start">
+			<span class="text-lg font-bold">
+				IoT
+			</span>
+		</div>
+		<div class="hidden px-2 mx-2 navbar-center lg:flex">
+			<div class="flex items-stretch">
+				<a class="btn btn-ghost btn-sm rounded-btn">
+					Home
+				</a>
+				<a class="btn btn-ghost btn-sm rounded-btn">
+					Portfolio
+				</a>
+				<a class="btn btn-ghost btn-sm rounded-btn">
+					About
+				</a>
+				<a class="btn btn-ghost btn-sm rounded-btn">
+					Contact
+				</a>
+			</div>
+		</div>
+		<div class="navbar-end">
+			<button class="btn btn-square btn-ghost">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>
+				</svg>
+			</button>
+			<button class="btn btn-square btn-ghost">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+				</svg>
+			</button>
+		</div>
+	</div>
+
+</nav>
+
+<body>
+	<div class="hero min-h-screen bg-base-200">
+		<div class="text-center hero-content">
+			<div class="max-w-md">
+				<h1 class="mb-5 text-5xl font-bold">
+					Hello there
+				</h1>
+				<p class="mb-5">
+					Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+				</p>
+				<button class="btn btn-primary">Get Started</button>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
