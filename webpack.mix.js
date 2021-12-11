00d2a1c1162a2c63c3d74d3963b2d53e3ef39cea
Muhammad Rifki Widadi <rifki.widadi29@gmail.com>
2021-12-11 20:25:32 +0700

    bundle: add laravel mix as styling compiler

---
+++ webpack.mix.js
@@ -0,0 +1,7 @@
+let mix = require('laravel-mix');
+
+mix.js("src/js/app.js", "public/js")
+  .postCss("src/css/app.css", "public/css", [
+      require("tailwindcss"),
+      require("autoprefixer")
+  ]);
